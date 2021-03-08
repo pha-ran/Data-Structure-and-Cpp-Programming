60ff840cd629984729515afe48fc58aa5f92d384
pha-ran <pha-ran@naver.com>
2021-03-09 02:15:41 +0900

    Exception Handling

--- 01_date_class.cpp
+++ 01_date_class.cpp
@@ -9,17 +9,23 @@ using std::string;
 using std::remove;
 
 class Date {
-public:
+  public:
 	Date(int year_ = 2000, int month_ = 1, int day_ = 1); // 인수가 없을경우 기본값으로 초기화
 	~Date() = default;
-	void setDate(int year_, int month_, int day_);
+	bool setDate(int year_, int month_, int day_);
 	static void coutCurrentDate();
 	int getAge();
 
-private:
+  private:
 	int year;
 	int month;
 	int day;
+	static const int leap[2][12];
+};
+
+const int Date::leap[2][12] = {
+		{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+		{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
 int main() {
@@ -28,7 +34,7 @@ int main() {
 	string dept, name;
 	Date birth_date;
 
-	cout << "환영합니다! 학과명, 학번, 이름, 생년월일(yyyy/mm/dd)을 입력하세요 > ";
+	cout << "환영합니다! 학과명, 학번, 이름, 생년월일(yyyy/mm/dd)을 입력하세요> ";
 
 	cin >> dept >> id;
 	cin.ignore(1, ','); // 입력 버퍼에서 1개 삭제, ','가 나오면 ','삭제 후 중지
@@ -38,15 +44,22 @@ int main() {
 	cin.ignore(1, '/');
 	cin >> d;
 
-	birth_date.setDate(y, m, d);
+	if (id < 0) { cout << "입력 오류" << endl; return 0; };
 
-	// 문자열에서 ',' 삭제
-	dept.erase(remove(dept.begin(), dept.end(), ','), dept.end());
-	name.erase(remove(name.begin(), name.end(), ','), name.end());
+	//입력값이 정상일때
+	if (birth_date.setDate(y, m, d)) {
 
-	cout << ">> " << dept << " " << id << " " << name << "님 " << y << "년" << m << "월" << d << "일생 ";
-	Date::coutCurrentDate();
-	cout << " 현재 " << birth_date.getAge() << "세입니다." << endl;
+		// 문자열에서 ',' 삭제
+		dept.erase(remove(dept.begin(), dept.end(), ','), dept.end());
+		name.erase(remove(name.begin(), name.end(), ','), name.end());
+
+		cout << ">> " << dept << " " << id << " " << name << "님 " << y << "년" << m << "월" << d << "일생 ";
+		Date::coutCurrentDate();
+		cout << " 현재 " << birth_date.getAge() << "세입니다." << endl;
+	}
+	else {
+		cout << "입력 오류" << endl;
+	}
 
 	return 0;
 }
@@ -57,10 +70,37 @@ Date::Date(int year_, int month_, int day_) :
 	day(day_)
 {}
 
-void Date::setDate(int year_, int month_, int day_) {
-	year = year_;
-	month = month_;
-	day = day_;
+bool Date::setDate(int year_, int month_, int day_) {
+
+	time_t current_time_sec = time(NULL);
+	struct tm current_time;
+	localtime_s(&current_time, &current_time_sec);
+
+	//입력 날짜가 현재 날짜보다 클 경우
+	if (year_ > current_time.tm_year + 1900 ||
+		(year_ == current_time.tm_year + 1900 && month_ > current_time.tm_mon + 1) ||
+		(year_ == current_time.tm_year + 1900 && month_ == current_time.tm_mon + 1 && day_ > current_time.tm_mday)
+		) {
+		return false;
+	}
+
+	//윤년 판별
+	bool leap_ = ((year_ % 4 == 0) && (year_ % 100 != 0)) || (year_ % 400 == 0);
+
+	//날짜가 정상 범위인지 판별
+	if (year_ >= 1900 &&
+		(month_ >= 1 && month_ <= 12) &&
+		(day_ >= 1 && day_ <= leap[leap_][month_ - 1])
+		) {
+		year = year_;
+		month = month_;
+		day = day_;
+
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 void Date::coutCurrentDate() {
@@ -76,5 +116,7 @@ int Date::getAge() {
 	struct tm current_time;
 	localtime_s(&current_time, &current_time_sec);
 
-	return current_time.tm_year + 1900 - year + 1; //한국 나이 계산
+	bool birth = (current_time.tm_mon + 1 > month || (current_time.tm_mon + 1 == month && current_time.tm_mday >= day));
+
+	return current_time.tm_year + 1900 - year - 1 + birth; //만 나이 계산
 }
