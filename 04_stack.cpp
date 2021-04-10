4729082b4a66f4e6aee3391424af810e859a9181
pha-ran <pha-ran@naver.com>
2021-04-10 20:23:15 +0900

    balanced parentheses

--- 04_stack.cpp
+++ 04_stack.cpp
@@ -20,6 +20,112 @@ class Stack {
 
 int main() {
 
+	Stack<char> formula(10), parentheses(10), temp(10);	//각각 수식, 괄호, 임시 저장용 스택
+	char c = '#';	//입력값을 받는 변수 초기화
+	int chk = 0;	//괄호 입력 여부 확인 변수 초기화
+
+	while (c = getchar()) {		//한 글자씩 대입
+		if (c == '#' || c == '\n') { break; }	//대입값이 # 또는 \n이면 입력 종료
+		
+		formula.Push(c);	//수식 스택에 추가
+		
+		if (c == '(' ||				//여는 괄호일때
+			c == '[' ||
+			c == '<' ||
+			c == '{'
+			) {
+			parentheses.Push(c);	//괄호 스택에 추가
+			chk++;					//괄호 입력 여부 확인
+		}
+		else if (					//닫는 괄호일때
+			c == ')' ||
+			c == ']' ||
+			c == '>' ||
+			c == '}'
+			) {
+
+			if (parentheses.isEmpty()) {	//닫는 괄호인데 괄호 스택이 비었을경우
+				for (int i = 0; formula.isEmpty() != 1; i++) {	//임시 스택에 수식 스택의 값 추가
+					temp.Push(formula.Top());
+					formula.Pop();
+				}
+
+				cout << "Error : ";
+
+				for (int i = 0; temp.isEmpty() != 1; i++) {		//수식 스택 출력
+					cout << temp.Top();
+					temp.Pop();
+				}
+
+				cout << endl;
+				cout << "Stack : empty";	//빈 스택임을 출력
+
+				return 0;	//프로그램 종료
+			}
+
+			if (					//괄호 스택의 top과 매칭이 될 경우
+				(parentheses.Top() == '(' && ')' == c) ||
+				(parentheses.Top() == '[' && ']' == c) ||
+				(parentheses.Top() == '<' && '>' == c) ||
+				(parentheses.Top() == '{' && '}' == c)
+				) {
+				parentheses.Pop();	//매칭된 괄호 제거
+			}
+			else {					//매칭이 안될 경우
+				break;				//반복문 종료
+			}
+		}
+	}
+
+	if (formula.isEmpty() || chk == 0) {	//수식 스택이 비어있거나 괄호 입력이 없을 경우
+		cout << "Error" << endl;
+		return 0;
+	}
+
+	if (parentheses.isEmpty()) {		//수식 스택이 비어있지 않고, 괄호도 입력되었으며 괄호 스택이 비었을 경우 (정상 입력인 경우)
+
+		for (int i = 0; formula.isEmpty() != 1; i++) {	//임시 스택에 수식 스택의 값 추가
+			temp.Push(formula.Top());
+			formula.Pop();
+		}
+
+		cout << "Correct! ";
+
+		for (int i = 0; temp.isEmpty() != 1; i++) {		//수식 스택 출력
+			cout << temp.Top();
+			temp.Pop();
+		}
+
+		cout << endl;
+	}
+	else {								//수식 스택이 비어있지 않지만 괄호 스택에 값이 있을 경우 (괄호 매칭에 실패한 경우)
+		for (int i = 0; formula.isEmpty() != 1; i++) {	//임시 스택에 수식 스택의 값 추가
+			temp.Push(formula.Top());
+			formula.Pop();
+		}
+
+		cout << "Error : ";
+
+		for (int i = 0; temp.isEmpty() != 1; i++) {		//수식 스택의 값 출력
+			cout << temp.Top();
+			temp.Pop();
+		}
+
+		cout << endl;
+		
+		for (int i = 0; parentheses.isEmpty() != 1; i++) {	//임시 스택에 괄호 스택의 값 추가
+			temp.Push(parentheses.Top());
+			parentheses.Pop();
+		}
+
+		cout << "Stack : ";
+
+		for (int i = 0; temp.isEmpty() != 1; i++) {		//괄호 스택의 값 출력
+			cout << temp.Top();
+			temp.Pop();
+		}
+	}
+
 	return 0;
 }
 
