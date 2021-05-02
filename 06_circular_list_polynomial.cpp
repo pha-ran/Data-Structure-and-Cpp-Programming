c59390456de72884e04154d266509c13657a7521
pha-ran <pha-ran@naver.com>
2021-05-02 23:42:05 +0900

    circular_list_polynomial

--- 06_circular_list_polynomial.cpp
+++ 06_circular_list_polynomial.cpp
@@ -85,10 +85,9 @@ public:
 	Polynomial() { list = new CircularListWithHeader(); }
 	void addTerm(const int c, const int e) { list->addNode(c, e); }		//항을 추가
 	const bool isEmpty() { return list->begin() == list->Head(); }
-	const CircularListNode sMultPoly(const int c, const int e);	//다항식의 단항 곱셈
+	const Polynomial sMultPoly(const int c, const int e);	//다항식의 단항 곱셈
 	const int evalPoly(const int c);			//다항식의 값을 계산
-	//다항식 덧셈 연산
-	const Polynomial operator+(const Polynomial& p);
+	const Polynomial operator+(const Polynomial& p);	//다항식 덧셈 연산
 	const Polynomial operator*(const Polynomial& p);	//다항식의 곱 연산
 	Polynomial& operator=(const Polynomial& p);			//다항식 대입 연산
 	friend ostream& operator<<(ostream& os, const Polynomial& p);	// 출력 연산자 << 오버로딩 (모든 다항식은 항이 )
@@ -102,7 +101,9 @@ int main() {
 	while (true) {
 		Polynomial a, b, c, d, t;
 		int x;
-		cout << ">Input polynomials a, b, c : ";	//(계수, 지수)형태의 정상정인 입력값만 처리, 이외는 무시 (항이 없을 경우 예외 처리)
+		cout << ">Input polynomials a, b, c : ";
+		//(계수, 지수)형태의 정상정인 입력값만 처리, 이외는 무시
+		//정상적인 입력과 비정상적인 입력이 혼합 시 정상적인 입력값만 처리, 항이 없을 경우 예외 처리
 		cin >> a;									//#입력시 프로그램 종료
 		cin >> b;
 		cin >> c;
@@ -117,7 +118,7 @@ int main() {
 		cout << "A(x) = " << a << endl;
 		cout << "B(x) = " << b << endl;
 		cout << "C(x) = " << c << endl;
-		/*
+
 		t = a * b;
 		d = t + c;
 		cout << "T(x) = " << t << endl;
@@ -134,7 +135,6 @@ int main() {
 
 		cout << "A*B+C = " << d.evalPoly(x);
 		cout << endl << endl;
-		*/
 	}
 
 	return 0;
@@ -145,7 +145,7 @@ ostream& operator<<(ostream& os, const Polynomial& p) {
 	for (CircularListWithHeader::iterator it = p.list->begin(); it != p.list->Head(); ++it) {
 		if (it != p.list->begin() && (*it).coef > 0) { os << "+"; } //두번째 항부터는 계수가 양수이면 + 출력
 
-		if ((*it).coef != 1 && (*it).exp != -1) { //계수가 1또는 -1이 아니면 계수를 출력
+		if ((*it).coef != 1 && (*it).coef != -1) { //계수가 1또는 -1이 아니면 계수를 출력
 			os << (*it).coef;
 		}
 		else if ((*it).coef == -1 && (*it).exp > 0) { os << "-"; } //계수가 -1이고 지수가 0보다 크면 -출력
@@ -189,7 +189,7 @@ istream& operator>>(istream& is, Polynomial& p) {
 		str = sm.suffix(); //문자열에서 다음 (계수,지수)형태 탐색
 	}
 
-	//p.sortList();	//입력받은 항들을 정렬 후 동류항 계산
+	p.sortList();	//입력받은 항들을 정렬 후 동류항 계산
 
 	return is;
 }
@@ -205,4 +205,116 @@ Polynomial& Polynomial::operator=(const Polynomial& p) {
 	}
 
 	return *this;
+}
+
+//항을 지수의 내림차순으로 정렬하고 동류항을 계산한 결과를 저장
+void Polynomial::sortList() {
+	CircularListWithHeader* sort = new CircularListWithHeader();
+	int max = 0;
+
+	for (CircularListWithHeader::iterator it = list->begin(); it != list->Head(); ++it) {
+		if ((*it).exp > max) { max = (*it).exp; }
+	}
+
+	max++;	//max는 최대 차수보다 항상 큼
+
+	for (CircularListWithHeader::iterator i = list->begin(); i != list->Head(); ++i) {	//항을 지수의 내림차순으로 정렬
+		int k = -1;
+
+		for (CircularListWithHeader::iterator j = list->begin(); j != list->Head(); ++j) {	//max보다 작은 최대 차수를 탐색
+			if ((*j).exp > k && (*j).exp < max) {
+				k = (*j).exp;
+			}
+		}
+
+		for (CircularListWithHeader::iterator j = list->begin(); j != list->Head(); ++j) {	//지수가 k인 모든 항을 sort에 추가
+			if (k == (*j).exp) {
+				sort->addNode((*j).coef, (*j).exp);
+			}
+		}
+
+		max = k;
+	}
+
+	CircularListWithHeader* temp = new CircularListWithHeader();
+
+	int sum = 0;	//임시 계수
+
+	for (CircularListWithHeader::iterator i = sort->begin(); i != sort->Head(); ++i) {	//sort의 모든 동류항을 계산
+		CircularListWithHeader::iterator j = i;
+		++j;
+
+		if (j != sort->Head()) {	//j가 헤드가 아닌 경우
+			if ((*i).exp == (*j).exp) {	//다음 노드가 동류항일 경우
+				sum += (*i).coef;	//임시 계수 증가
+			}
+			else {	//다음 노드가 동류항이 아닐 경우
+				if (sum + (*i).coef) {	//계수가 0이 아닐 경우
+					temp->addNode(sum + (*i).coef, (*i).exp);	//temp에 노드 추가
+					sum = 0;
+				}
+			}
+		}
+		else {	//j가 헤드인경우 (i가 마지막 노드인 경우)
+			if (sum + (*i).coef) {	//계수가 0이 아닐 경우
+				temp->addNode(sum + (*i).coef, (*i).exp);	//temp에 노드 추가
+			}
+		}
+	}
+
+	list = temp;	//정렬후 동류항이 계산된 다항식을 list에 대입
+}
+
+//다항식 덧셈 연산
+const Polynomial Polynomial::operator+(const Polynomial& p) {
+	Polynomial temp;
+
+	for (CircularListWithHeader::iterator it = list->begin(); it != list->Head(); ++it) {
+		temp.addTerm((*it).coef, (*it).exp);	//this의 모든 항 추가
+	}
+
+	for (CircularListWithHeader::iterator it = p.list->begin(); it != p.list->Head(); ++it) {
+		temp.addTerm((*it).coef, (*it).exp);	//p의 모든 항 추가
+	}
+
+	temp.sortList();	//정렬 후 동류항 계산
+
+	return temp;
+}
+
+//다항식의 단항 곱셈
+const Polynomial Polynomial::sMultPoly(const int c, const int e) {
+	Polynomial temp;	//결과값을 저장할 변수
+
+	for (CircularListWithHeader::iterator it = list->begin(); it != list->Head(); ++it) {
+		temp.addTerm((*it).coef * c, (*it).exp + e);	//cx^e와 this를 곱한 결과를 temp에 저장
+	}
+
+	temp.sortList();	//정렬 후 동류항 계산
+
+	return temp;
+}
+
+//다항식의 곱 연산
+const Polynomial Polynomial::operator*(const Polynomial& p) {
+	Polynomial temp;	//결과값을 저장할 변수
+
+	for (CircularListWithHeader::iterator it = p.list->begin(); it != p.list->Head(); ++it) {
+		temp = temp + sMultPoly((*it).coef, (*it).exp);		//p의 각 항들을 this와 단항 곱셈 후 temp와 덧셈
+	}
+
+	temp.sortList();	//정렬 후 동류항 계산
+
+	return temp;
+}
+
+//다항식의 값을 계산
+const int Polynomial::evalPoly(const int c) {
+	int y = 0; //결과값을 저장할 변수
+
+	for (CircularListWithHeader::iterator it = list->begin(); it != list->Head(); ++it) {
+		y += (*it).coef * (int)pow(c, (*it).exp);	//각 항의 값을 계산하여 결과값에 더한다.
+	}
+
+	return y; //결과값을 반환
 }
